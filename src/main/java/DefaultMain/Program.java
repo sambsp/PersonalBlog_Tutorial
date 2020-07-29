364c64dd6efdf4815beb510fc56d393957ca9bde
joeshro <910833080@qq.com>
2020-07-29 12:46:08 +0800

    chapter_21

--- src/main/java/DefaultMain/Program.java
+++ src/main/java/DefaultMain/Program.java
@@ -1,11 +1,14 @@
 package DefaultMain;
 
+import Database.DatabaseMan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Program  {
     public static void main(String[] args) {
+        DatabaseMan.Instance().Init();
+
         System.out.println("Spring boot application start");
         SpringApplication.run(Program.class, args);
     }

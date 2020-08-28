5ea0ce90023bd4a87e222a85cca32b8f2f95d2ea
joeshro <910833080@qq.com>
2020-08-28 17:13:23 +0800

    update

--- src/main/resources/static/javascript/catalog.js
+++ src/main/resources/static/javascript/catalog.js
@@ -4,7 +4,32 @@ function onAddCatalog() {
 
 $('#catalog-input').on('keyup', function(e) {
     if (e.keyCode == 13) {
-        console.log("hello world");
+        var catalog = $('#catalog-input').val().trim();
+
+        var data = {'catalog': catalog}
+
+        if (catalog != '') {
+            $.ajax({
+                type: 'post',
+                async: true,
+                data: JSON.stringify(data),
+                url: document.location.origin + '/api/addcatalog',
+                dataType:'json',
+                contentType: "application/json; charset=utf-8",
+                success: function(data) {
+                    Toast.fire({
+                        type: 'success',
+                        title: '已经添加'
+                    });
+                },
+                error: function (xhr) {
+                    Toast.fire({
+                        type: 'error',
+                        title: "Ajax 发生错误: " + xhr.responseText
+                    });
+                }
+            });
+        }
     }
 });
 
